22085e3ebfd919c3c8ed16db0cb6447876fee1f1
limy <lmy@qiaosports.com>
2019-09-23 14:43:20 +0800

    button

--- library_typeface/src/main/java/com/lmy/library/typeface/SiYuanFontButton.java
+++ library_typeface/src/main/java/com/lmy/library/typeface/SiYuanFontButton.java
@@ -3,41 +3,31 @@ package com.lmy.library.typeface;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Typeface;
 import android.util.AttributeSet;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 
 /**
- * 思源字体
- * Create by 2019/9/21
+ * Button
+ * Create by 2019/9/23
  * Author lmy
  */
 @SuppressLint("AppCompatCustomView")
-public class SiYuanFontTextView extends TextView {
+public class SiYuanFontButton  extends Button {
     private Context mContext;
-
-    /**
-     *   <enum name="RegularSan" value="1"/>
-     *             <enum name="MediumAir" value="2"/>
-     *             <enum name="BookAir" value="3"/>
-     *             <enum name="MediumSan" value="4"/>
-     *             <enum name="NormalSan" value="5"/>
-     */
-
 
     private SiYuanFontManger siYuanFontManger;
 
-    public SiYuanFontTextView(Context context) {
+    public SiYuanFontButton(Context context) {
         this(context, null);
     }
 
-    public SiYuanFontTextView(Context context, @Nullable AttributeSet attrs) {
+    public SiYuanFontButton(Context context, @Nullable AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public SiYuanFontTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public SiYuanFontButton(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         mContext = context;
         siYuanFontManger = new SiYuanFontManger(mContext);
@@ -46,6 +36,7 @@ public class SiYuanFontTextView extends TextView {
 
     private void initTypeView(AttributeSet attrs) {
         setIncludeFontPadding(false);
+        @SuppressLint("CustomViewStyleable")
         TypedArray typedArray = mContext.obtainStyledAttributes(attrs, R.styleable.SiYuanFontTextView);
         int textType = typedArray.getInt(R.styleable.SiYuanFontTextView_fontTypeface,SiYuanFontManger.SNA_MEDIUM);
         switch (textType) {
---
+++ library_typeface/src/main/java/com/lmy/library/typeface/SiYuanFontManger.java
@@ -0,0 +1,68 @@
+package com.lmy.library.typeface;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+/**
+ * 思源字体
+ * Create by 2019/9/23
+ * Author lmy
+ */
+public class SiYuanFontManger {
+
+    private final Context mContext;
+
+    public static final int REGULAR = 1;
+    public static final int AIR_MEDIUM = 2;
+    public static final int AIR_BOOK = 3;
+    public static final int SNA_MEDIUM = 4;
+    public static final int SNA_NORMAL = 5;
+
+    public SiYuanFontManger(Context mContext){
+        this.mContext = mContext;
+    }
+
+    private Typeface regularTypeface;
+    private Typeface airMediumTypeface;
+    private Typeface airBookTypeface;
+    private Typeface sanMediumTypeface;
+    private Typeface sanNormalTypeface;
+
+
+    Typeface getTypefaceR() {
+        if (regularTypeface == null) {
+            regularTypeface = Typeface.createFromAsset(mContext.getAssets(), "fonts/NotoSansHans-Regular.otf");
+        }
+        return regularTypeface;
+    }
+
+    Typeface getTypefaceAM() {
+        if (airMediumTypeface == null) {
+            airMediumTypeface = Typeface.createFromAsset(mContext.getAssets(), "fonts/AvenirLTStd-Medium.otf");
+        }
+        return airMediumTypeface;
+    }
+
+    Typeface getTypefaceAB() {
+        if (airBookTypeface == null) {
+            airBookTypeface = Typeface.createFromAsset(mContext.getAssets(), "fonts/AvenirLTStd-Book.otf");
+        }
+        return airBookTypeface;
+    }
+
+    Typeface getTypefaceSM() {
+        if (sanMediumTypeface == null) {
+            sanMediumTypeface = Typeface.createFromAsset(mContext.getAssets(), "fonts/NotoSansHans-Medium.otf");
+        }
+        return sanMediumTypeface;
+    }
+
+    Typeface getTypefaceSN() {
+        if (sanNormalTypeface == null) {
+            sanNormalTypeface = Typeface.createFromAsset(mContext.getAssets(), "fonts/SourceHanSansCN-Normal-2.otf");
+        }
+        return sanNormalTypeface;
+    }
+
+}
